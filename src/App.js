03e7c488b02f55149da1c751bbfd30dba76ed86a
megha2601 <109163494+megha2601@users.noreply.github.com>
2023-05-31 19:09:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import "./index.css";
+import React, { useState } from "react";
+
+function App() {
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  let calcBmi = (event) => {
+    event.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      alert("Please enter a valid weight and height");
+    } else {
+      let bmi = (weight / (height * height)) * 703;
+      setBmi(bmi.toFixed(1));
+
+      if (bmi < 25) {
+        setMessage("You are underweight");
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage("You are a healthy");
+      } else {
+        setMessage("You are overweight");
+      }
+    }
+  };
+
+  let imgSrc;
+
+  if (bmi < 1) {
+    imgSrc = null;
+  } else {
+    if (bmi < 25) {
+      imgSrc = require("../src/images/underweight.PNG");
+    } else if (bmi >= 25 && bmi < 30) {
+      imgSrc = require("../src/images/healthy.PNG");
+    } else {
+      imgSrc = require("../src/images/overweight.PNG");
+    }
+  }
+
+  let reload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (lbs)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input
+              value={height}
+              onChange={(event) => setHeight(event.target.value)}
+            />
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline" onClick={reload} type="submit">
+              Reload
+            </button>
+          </div>
+        </form>
+
+        <div className="center">
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className="img-container">
+          <img src={imgSrc} alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
